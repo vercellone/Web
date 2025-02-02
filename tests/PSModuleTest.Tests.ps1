46218d98b91d044cd6252aa659efaef0601983a3
Marius Storhaug <marstor@hotmail.com>
2025-02-02 09:10:44 +0100

    Initial commit

---
+++ tests/PSModuleTest.Tests.ps1
@@ -0,0 +1,14 @@
+﻿Describe 'Module' {
+    It 'Function: Get-PSModuleTest' {
+        Get-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
+    }
+    It 'Function: New-PSModuleTest' {
+        New-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
+    }
+    It 'Function: Set-PSModuleTest' {
+        Set-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
+    }
+    It 'Function: Test-PSModuleTest' {
+        Test-PSModuleTest -Name 'World' | Should -Be 'Hello, World!'
+    }
+}
